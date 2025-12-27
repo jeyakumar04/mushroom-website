836198f46faec29e22723a81d8d4aa1c48ef4eea
jeyakumar04 <tjjeyakumar007@gmail.com>
2025-12-27 12:20:42 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Gallery from './pages/Gallery';
+import Blog from './pages/Blog';
+import Training from './pages/Training';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>TJP MUSHROOM FARMING</h1>
-    </div>
-  )
+    <Router>
+      {/* Navbar inga thaan varum */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/training" element={<Training />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div >
+      <h1>🍄 Welcome to Mushroom Farm!</h1>
+      <p>Home Page ippo work aaguthu.</p>
+    </div>
+  );
+};
+
+export default Home;
